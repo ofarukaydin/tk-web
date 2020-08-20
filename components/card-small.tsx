5922a1fc69b5cef552f2a39932fb5c68e74ce255
Faruk Aydin <hayspat@gmail.com>
2020-08-20 17:59:53 +0300

    fix card

--- components/card-small.tsx
+++ components/card-small.tsx
@@ -7,10 +7,12 @@ type PropTypes = {
 
 const SmallCard = ({ imageSrc = "/cherry.png", title }: PropTypes) => {
   return (
-    <div tw="rounded-xl shadow-tk2 w-40 h-48">
-      <div tw="p-4 h-full flex flex-col justify-between">
-        <img tw="h-full" src={imageSrc} alt="product" />
-        <div tw="text-lg leading-5 font-medium text-center">{title}</div>
+    <div tw="rounded-xl shadow-tk2">
+      <div tw="p-4">
+        <div tw="flex justify-center items-center">
+          <img src={imageSrc} alt="product" />
+        </div>
+        <div tw="mt-2 text-lg leading-5 font-medium text-center">{title}</div>
       </div>
     </div>
   );
